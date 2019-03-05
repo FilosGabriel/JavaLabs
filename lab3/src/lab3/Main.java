ba6bdab5e206654d5cca519f1cdba7082024bc2e
danibranch <danybrench33@gmail.com>
2019-03-05 22:06:40 +0200

    Am adaugat getNodes()

--- lab3/src/lab3/Main.java
+++ lab3/src/lab3/Main.java
@@ -7,5 +7,14 @@ public class Main {
     {
         Church c = new Church("Voroneti", LocalTime.of(12, 0), LocalTime.of(16, 0));
         Hotel v1 = new Hotel("California");
+        Museum v2 = new Museum("Museum of Failure");
+        Museum v3 = new Museum("ASimple museum");
+        TravelMap map = new TravelMap();
+        map.addNode(v1);
+        map.addNode(v2);
+        map.addEdge(v1, v2, 15);
+        map.addNode(c);
+        map.addNode(v3);
+        System.out.println(map.getNodes());
     }
 }
